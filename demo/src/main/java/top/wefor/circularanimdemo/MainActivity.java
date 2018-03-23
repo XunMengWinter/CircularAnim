ae84943545abd677a0d4d602f27a624f8ae706a9
Winter <XunMengWinter@gmail.com>
2018-03-23 13:58:12 +0800

    demo add deployAnimator(...)

--- demo/src/main/java/top/wefor/circularanimdemo/MainActivity.java
+++ demo/src/main/java/top/wefor/circularanimdemo/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
